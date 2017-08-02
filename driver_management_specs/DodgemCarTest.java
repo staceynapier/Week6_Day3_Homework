57cb957fa06bd35490479be51f9136f78ccf0717
staceynapier <staceynapier86@gmail.com>
2017-08-02 15:52:50 +0100

    initial commit

---
+++ driver_management_specs/DodgemCarTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import driver_management.*;
+
+public class DodgemCarTest {
+
+  DodgemCar car
+
+  @Before
+  public void before() {
+    car = new DodgemCar();
+  }
+
+  @Test
+  public void driveDistanceReturnsTime() {
+    assertEquals(100, car.driveDistance() );
+  }
+
+}
